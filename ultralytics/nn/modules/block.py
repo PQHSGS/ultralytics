72b7309f37b29b46e840bef883a6b14373bb7715
Trần Phong Quân <165889856+PQHSGS@users.noreply.github.com>
2025-11-05 18:52:52 +0700

    new model arch

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -70,6 +70,39 @@ class SimFusion_3in(nn.Module):
         out = torch.cat([x_t, x_s, x_m], 1)
         return out
 
+class dilation_block(nn.Module):
+    def __init__(self, c1, c2, e=0.5):
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c2, 1, 1)
+
+        self.cv2 = Conv(c_, c_, 3, 1)
+        self.cv3 = Conv(c_, c_, 3, 1)
+        self.cv4 = Conv(c_, c_, 3, 1)
+        self.cv5 = Conv(c_, c_, 3, 1)
+        self.cv6 = Conv(c_, c_, 3, 1)
+
+        self.cv8 = Conv(c_, c_, 3, 1)
+
+        self.cv7 = Conv(c_, c_, 3, 1, d=3)
+
+        self.cv9 = Conv(5*c_, c2, 1, 1)
+
+    def forward(self, x):
+
+        y = list(self.cv1(x).chunk(2, 1))
+
+        block1 = self.cv3(self.cv2(y[-1]))
+        block2 = self.cv5(self.cv4(block1))
+        block3 = self.cv7(self.cv6(block2))
+        y[0] = self.cv8(y[0])
+
+        y.extend([block1,block2,block3])
+
+        out = self.cv9(torch.cat(y,1))
+
+        return out
+        
 class DFL(nn.Module):
     """Integral module of Distribution Focal Loss (DFL).
 
